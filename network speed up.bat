923c53ae756b5f17b84d32bf58db161d9689d76a
geek28 <cubanazo40@yahoo.es>
2026-08-14 13:17:17 -0700

    network speed up

---
+++ network speed up.bat
@@ -0,0 +1,23 @@
+netsh int ip reset C:\RestLog.txt  
+ipconfig /release  
+ipconfig /renew 
+ipconfig /flushdns 
+ipconfig /displaydns 
+ipconfig /registerdns 
+netsh winsock reset 
+netsh int tcp set global chimney=enabled
+netsh int tcp set global autotuninglevel=normal
+netsh int tcp set global dca=enabled  
+netsh int tcp set global netdma=enabled
+netsh int tcp set global ecncapability=enabled
+netsh int tcp set global congestionprovider=ctcp
+netsh interface tcp set global rss=enabled 
+netsh interface tcp set heuristics disabled 
+netsh int tcp set global initialRto=2000 
+netsh int tcp set global fastopen=enabled
+netsh int tcp set global hystart=enabled 
+netsh int tcp set global maxsynretransmissions=2 
+netsh int tcp set global timestamps=disabled 
+netsh int tcp set global nonsackrttresiliency=disabled
+netsh int ipv4 set dynamicport tcp start=10000 num=3000 
+netsh int ipv6 set dynamicport tcp start=10000 num=3000
